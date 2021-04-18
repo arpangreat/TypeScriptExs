1056c9849d09129d17f56c3f2e8ca39650beea8b
arpangreat <arpanthegreat41@gmail.com>
2021-04-18 21:31:03 +0530

    webpack tutorial

---
+++ WebPackTutorial/app/index.js
@@ -0,0 +1,5 @@
+const myArray = ["Rampurhat", "Kurumgram", "Kolkata"];
+
+console.log(myArray);
+
+export default myArray;
---
+++ WebPackTutorial/dist/bundle.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log(["Rampurhat","Kurumgram","Kolkata"])})();
